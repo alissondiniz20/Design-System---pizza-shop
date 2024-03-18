0cd54e4d2b1c71aa5c5ddf879db57d5c692c776f
Alisson Diniz <71110962+alissondiniz20@users.noreply.github.com>
2024-03-18 15:34:23 -0300

    dashboard page

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { TableRow, TableCell } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return(
     <TableRow> 
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
+        
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         weqr9we9r8we9r5w6598656
@@ -18,12 +27,7 @@ export function OrderTableRow() {
         há 15 minutos
       </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-md text-muted-foregrounds">
-            Pendente
-          </span>
-        </div>
+        
       </TableCell>
       <TableCell className="font-medium">
         Alisson Barbosa Diniz
