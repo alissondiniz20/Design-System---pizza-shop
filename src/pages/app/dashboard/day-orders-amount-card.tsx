25f0535355a6d4710399e2948ecdaf0ea294f393
Alisson Diniz <71110962+alissondiniz20@users.noreply.github.com>
2024-03-19 15:41:26 -0300

    popular products chart

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Utensils } from "lucide-react";
+import {  Utensils } from "lucide-react";
 
 export function DayOrdersAmountCard() {
   return (
